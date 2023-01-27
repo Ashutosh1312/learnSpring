3f54a9c687391d8f27e1f96ffefa0ce2fc23ff88
Ashutosh1312 <122602412+Ashutosh1312@users.noreply.github.com>
2023-01-27 13:35:18 +0530

    Update TestCode.java
    code updated

--- learn-spring/src/main/java/com/concrete/TestCode.java
+++ learn-spring/src/main/java/com/concrete/TestCode.java
@@ -3,6 +3,10 @@ package com.concrete;
 public class TestCode {
 public static void main(String[] args) {
 	System.out.println("Hello world");
+	int a=10;int b =20;
+	int c =  a+b;
+	System.out.println("addition of a n b :"+c);
+	
 }
 
 }
